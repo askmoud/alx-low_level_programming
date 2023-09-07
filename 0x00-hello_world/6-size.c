c0f9d739616c381b3ea2952920e3a6c717037d1a
askmoud <hamzaaskmoud27@gmail.com>
2023-09-07 18:13:00 +0100

    Size code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Programming
+ * Return: return value is 0
+*/ 
+int main(voide)
+{
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of char: %lu byte(s)\n", sizeof(int));
+printf("Size of char: %lu byte(s)\n", sizeof(long));
+printf("Size of char: %lu byte(s)\n", sizeof(long long));
+printf("Size of char: %lu byte(s)\n", sizeof(float));
+}
+return (0);
