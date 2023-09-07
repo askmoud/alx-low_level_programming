e8eafc741bf81023a8455041ce53e9bbdcdf31bf
askmoud <hamzaaskmoud27@gmail.com>
2023-09-07 18:19:05 +0100

    Size script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,13 @@
 /**
 * main - Programming
 * Return: return value is 0
-*/ 
-int main(voide)
+*/
+int main(void)
 {
 printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of char: %lu byte(s)\n", sizeof(int));
-printf("Size of char: %lu byte(s)\n", sizeof(long));
-printf("Size of char: %lu byte(s)\n", sizeof(long long));
-printf("Size of char: %lu byte(s)\n", sizeof(float));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
