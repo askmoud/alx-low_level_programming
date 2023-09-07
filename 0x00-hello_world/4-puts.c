b2929bee39e5e0be364f7d0eccca18afdd37dd67
askmoud <hamzaaskmoud27@gmail.com>
2023-09-07 17:19:02 +0100

    First c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+* main - Programming
+* Return: return value is 0
+*/ 
 
-int main (){
+int main(void)
+{
 
-    puts("Programming is like building a multilingual puzzle\n");
-
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
