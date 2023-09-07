fddfa9452f45b22bf94309e2932f014fb670e1d1
askmoud <hamzaaskmoud27@gmail.com>
2023-09-07 17:40:56 +0100

    Printf c code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * * main - Programming
- * * Return: return value is 0
- * */
+* main - Programming
+* Return: return value is 0
+*/
+
 int main (void){
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
