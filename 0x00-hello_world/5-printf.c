08804b9aeb3c333fb84533378a917a16b0552330
askmoud <hamzaaskmoud27@gmail.com>
2023-09-07 17:48:42 +0100

    Printf script code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
 * main - Programming
 * Return: return value is 0
 */
-
-int main (void){
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
