cf810613da5eea51e7d72fb4244300325e57fa96
askmoud <hamzaaskmoud27@gmail.com>
2023-09-08 13:21:16 +0100

    Lowercase uppercase alphabets code

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: value is 0
+*/
+
+int main(void)
+{
+char lowercase = 'a';
+char uppercase = 'A';
+
+while
+	(lowercase <= 'z');
+{
+	putchar(lowercase);
+	lowercase++;
+}
+
+while
+	(uppercase <= 'Z');
+{
+	putchar(uppercase);
+	uppercase++;
+}
+
+putchar('\n');
+
+
+return (0);
+}
