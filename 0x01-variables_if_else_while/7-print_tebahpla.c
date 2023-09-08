df9a21282dd2ff22040338bc5a89aabe3fa1b4d1
askmoud <hamzaaskmoud27@gmail.com>
2023-09-08 17:26:44 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - funciton
+ * Return: return value is 0
+ *
+*/
+int main(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i--;
+}
+putchar('\n');
+return (0);
+}
