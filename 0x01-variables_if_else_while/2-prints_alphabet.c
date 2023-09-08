b90ddd1c5f8e699e7907ce60579f1bc0f393a504
askmoud <hamzaaskmoud27@gmail.com>
2023-09-08 12:59:13 +0100

    Alphabet code

---
+++ 0x01-variables_if_else_while/2-prints_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: value is 0
+*/
+
+int main(void)
+{
+	char letter ='a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+return 0;
+
+
+
+}
