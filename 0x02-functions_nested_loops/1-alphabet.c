6343a5a96c59848ea2cafc183aee4f5cc146f3b9
askmoud <hamzaaskmoud27@gmail.com>
2023-09-12 11:33:48 +0100

    Changing directory name

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entery point
+ * Description: 'prints alphabet in lowercase'
+ * Return: value is 0
+*/
+
+void print_alphabet(void)
+{
+	int i;
+	for ( i = 97; i <= 122; i++ )
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
