e1db3d467d61d2e38c2a3c4bdc09a1b2927afd06
askmoud <hamzaaskmoud27@gmail.com>
2023-09-18 18:36:08 +0100

    Swap code

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* swap_int - function
+* @a: entery point
+* @b: enetery point
+* Return: return value is 0
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
