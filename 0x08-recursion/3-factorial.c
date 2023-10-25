8e290343352ecdcf0a14416895816e13d277d46e
askmoud <hamzaaskmoud27@gmail.com>
2023-10-25 16:36:16 +0100

    Recursive factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - function that returns factorial of a given num
+ * @n: entery point
+ * Return: no value
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
