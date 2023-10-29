b06b89316aa5391599ed7dd1acceee234a167ab5
askmoud <hamzaaskmoud27@gmail.com>
2023-10-29 14:49:42 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,11 @@ int main(int argc, char *argv[])
 	{
 	for (c = argv[argc]; *c; c++)
 	{
-		if (*c < '0' || *c > '9')
+		if (*c == 0)
+		{
+			return (0);
+		}
+		else if (*c < '0' || *c > '9')
 		{
 			printf("Error\n");
 			return (1);
