738b5982dd8e339091906c9879e56be00629e68a
askmoud <hamzaaskmoud27@gmail.com>
2023-10-29 13:09:15 +0100

    Argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: command line arguments
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("%s\n", argv[0]);
+	return (0);
+}
